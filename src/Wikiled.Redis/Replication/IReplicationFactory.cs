d5a78f6d3500b5690a1a2f58638ac51cce9f1daf
Andrius <andrius@wikiled.com>
2017-08-07 11:03:04 +0100

    Expose interface function

--- src/Wikiled.Redis/Replication/IReplicationFactory.cs
+++ src/Wikiled.Redis/Replication/IReplicationFactory.cs
@@ -1,10 +1,14 @@
 using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
 using Wikiled.Redis.Logic;
 
 namespace Wikiled.Redis.Replication
 {
     public interface IReplicationFactory
     {
+        Task<ReplicationProgress> Replicate(DnsEndPoint master, DnsEndPoint slave, CancellationToken token);
+
         IReplicationManager StartReplicationFrom(IRedisMultiplexer master, IRedisMultiplexer slave);
     }
 }
