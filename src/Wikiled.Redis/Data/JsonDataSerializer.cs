6f6b5aaa59ec770a7804525f943e64c53751fd3a
Andrius Mudinas <keistokas@gmail.com>
2019-10-24 11:05:06 +0100

    Adding Json serializer

---
+++ src/Wikiled.Redis/Data/JsonDataSerializer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Wikiled.Common.Utilities.Serialization;
+
+namespace Wikiled.Redis.Data
+{
+    public class JsonDataSerializer : IDataSerializer
+    {
+        private readonly IJsonSerializer jsonSerializer;
+
+        public JsonDataSerializer(IJsonSerializer jsonSerializer)
+        {
+            this.jsonSerializer = jsonSerializer ?? throw new ArgumentNullException(nameof(jsonSerializer));
+        }
+
+        public byte[] Serialize<T>(T instance)
+        {
+            return jsonSerializer.SerializeArray(instance);
+        }
+
+        public T Deserialize<T>(byte[] data)
+        {
+            return jsonSerializer.Deserialize<T>(data);
+        }
+
+        public object Deserialize(Type type, byte[] data)
+        {
+            return jsonSerializer.Deserialize(data);
+        }
+    }
+}
