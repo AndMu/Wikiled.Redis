2e28441f1d14354ec61f79d7d19cf5b186b9710d
Andrius Mudinas <keistokas@gmail.com>
2022-03-30 07:01:39 +0100

    Packages upgrade

--- src/Wikiled.Redis.UnitTests/Serialization/ObjectSetSerializationTests.cs
+++ src/Wikiled.Redis.UnitTests/Serialization/ObjectSetSerializationTests.cs
@@ -24,7 +24,7 @@ namespace Wikiled.Redis.UnitTests.Serialization
         [Test]
         public void Construct()
         {
-            ConstructorHelper.ConstructorMustThrowArgumentNullException<ObjectHashSetSerialization<Identity>>(true);
+            ConstructorHelper.ConstructorMustThrowArgumentNullException<ObjectHashSetSerialization<Identity>>();
         }
 
         [Test]
